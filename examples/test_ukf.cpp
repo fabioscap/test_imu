98e78d7b210c46390bb5ff4392f8f3c53c0e4429
Fabio Scaparro <fabiovt_99@hotmail.it>
2023-10-11 18:34:05 +0200

    fixed bug

--- examples/test_ukf.cpp
+++ examples/test_ukf.cpp
@@ -5,45 +5,36 @@ using namespace test_imu;
 
 using UKFType = UKF<core::Matrix3f, 3>;
 
-namespace test_imu {
-  template <>
-  UKFType::StateType boxplus(const UKFType::StateType& from, const UKFType::TangentType& dsp) {
-    core::Matrix3f R = from * core::geometry3d::expMapSO3(dsp);
-    core::fixRotation(R);
-    return R;
-  }
-  template <>
-  UKFType::TangentType boxminus(const UKFType::StateType& from, const UKFType::StateType& to) {
-    core::Matrix3f R = from.transpose() * to;
-    return core::geometry3d::logMapSO3(R.eval());
-  }
-} // namespace test_imu
+core::Matrix3f boxplusSO3(const core::Matrix3f& from, const core::Vector3f& dsp) {
+  core::Matrix3f R = from * core::geometry3d::expMapSO3(dsp);
+  core::fixRotation(R);
+  return R;
+}
+core::Vector3f boxminusSO3(const core::Matrix3f& from, const core::Matrix3f& to) {
+  core::Matrix3f R = from.transpose() * to;
+  return core::geometry3d::logMapSO3(R.eval());
+}
+core::Vector3f boxplusVector(const core::Vector3f& from, const core::Vector3f& dsp) {
+  return from + dsp;
+}
+core::Vector3f boxminusVector(const core::Vector3f& from, const core::Vector3f& to) {
+  return from - to;
+}
 
 int main() {
   // to unscented and back
 
-  UKFType ukf;
+  /*   UKFType ukf(boxplusSO3, boxminusSO3);
 
-  UKFType::StateType mean = UKFType::StateType::Identity();
+    UKFType::StateType mean = Ry(0.3f);
 
-  UKFType::CovType cov = UKFType::CovType::Identity();
-  cov(0, 0)            = 10;
+    UKFType::CovType cov = UKFType::CovType::Identity();
+    ukf.toUnscented(mean, cov);
 
-  ukf.toUnscented(mean, cov);
+    UKFType::StateType mean_rec;
+    UKFType::CovType cov_rec;
+    ukf.toMeanCov(mean_rec, cov_rec);
 
-  UKFType::StateType mean_rec = UKFType::StateType::Identity();
-  UKFType::CovType cov_rec    = UKFType::CovType::Identity();
-  ukf.toMeanCov(mean, cov);
-
-  std::cout << "sigma points:\n";
-  for (size_t i = 0; i < ukf.points_.size(); ++i) {
-    std::cout << "-" << i << "-\n" << ukf.points_.at(i) << "\n";
-  }
-
-  std::cout << "mean: \n";
-  std::cout << mean_rec << "\n";
-  std::cout << "covariance: \n";
-  std::cout << cov_rec << "\n";
-
-  // pass through a function
+    std::cout << "original: \n" << mean << "\n---\n" << cov << "\n" << std::endl;
+    std::cout << "reconstructed: \n" << mean_rec << "\n---\n" << cov_rec << std::endl; */
 }
--- src/common/ukf.h
+++ src/common/ukf.h
@@ -3,14 +3,10 @@
 
 namespace test_imu {
 
-  template <typename StateType, typename TangentType>
-  inline TangentType boxminus(const StateType& from, const StateType& to) {
-    throw std::runtime_error("boxminus not implemented");
-  }
-  template <typename StateType, typename TangentType>
-  inline StateType boxplus(const StateType& from, const TangentType& disp) {
-    throw std::runtime_error("boxplus not implemented");
-  }
+  template <int dim>
+  using TangentType_ = core::Vector_<float, dim>;
+
+  // Only what is needed for prediction. srrg2_solver does the update.
 
   template <typename StateType_, int state_dim_>
   class UKF {
@@ -22,59 +18,71 @@ namespace test_imu {
     using TangentType = core::Vector_<float, state_dim>;
     using CovType     = core::MatrixN_<float, state_dim>;
 
-    void toMeanCov(StateType& mean, CovType& cov) const {
+    void toMeanCov(StateType_& mean, CovType& cov) const {
       // compute chart point
-      const StateType& sigma_0 = points_[0];
+      const StateType_& sigma_0 = points_[0];
 
       // compute tangent mean displacement
       TangentType displacement = TangentType::Zero();
-      for (size_t i = 1; i < points_.size(); ++i)
-        displacement -= w_i_ * boxminus<StateType, TangentType>(sigma_0, points_.at(i));
+      for (size_t i = 1; i < points_.size(); ++i) {
+        const StateType_& Xi = points_.at(i);
+        displacement -= wi_ * boxminus(sigma_0, Xi);
+      }
 
       // add it to the mean
       mean = boxplus(sigma_0, displacement);
 
       // covariance lives in tangent space
-      for (size_t i = 0; i < points_.size(); ++i) {
-        const TangentType err = boxminus<StateType, TangentType>(mean, points_.at(i));
-        cov += err * err.transpose();
+      cov.setZero();
+      TangentType err = boxminus(mean, points_.at(0));
+      cov += wc0_ * err * err.transpose();
+
+      for (size_t i = 1; i < points_.size(); ++i) {
+        TangentType err = boxminus(mean, points_.at(i));
+        cov += wi_ * err * err.transpose();
       }
     }
-    void toUnscented(const StateType& mean, const CovType& cov) {
+    void toUnscented(const StateType_& mean, const CovType& cov) {
       points_[0] = mean;
 
       // mean weight
-      w_m_0 = lambda_ / (state_dim + lambda_);
+      wm0_ = lambda_ / (state_dim + lambda_);
       // covariance weight
-      w_c_0 = w_m_0 + (1 - alpha_ * alpha_ + beta_);
+      wc0_ = wm0_ + (1 - alpha_ * alpha_ + beta_);
       // weight i
-      w_i_ = 0.5 / (state_dim + lambda_);
+      wi_ = 0.5 / (state_dim + lambda_);
 
-      // perform cholesky decomposition
       // Perform Cholesky decomposition
       Eigen::LLT<Eigen::MatrixXf> llt((state_dim + lambda_) * cov);
 
       if (!llt.info() == Eigen::Success)
-        throw std::runtime_error("UKF::toUnscented| cholesky decomposition failed");
+        throw std::runtime_error("UKF::toUnscented| Cholesky decomposition failed");
 
       CovType L = llt.matrixL(); // should be symmetric
       for (size_t i = 0; i < state_dim; ++i) {
         const TangentType Li = L.col(i);
         points_[2 * i + 1]   = boxplus(mean, Li);
-        points_[2 * i + 2]   = boxplus(mean, Li);
+        points_[2 * i + 2]   = boxplus(mean, -Li);
       }
     }
 
     // protected:
-    std::vector<StateType> points_ = std::vector<StateType>(N);
-    float w_m_0, w_c_0, w_i_;
+    std::vector<StateType_> points_ = std::vector<StateType_>(N);
+    float wm0_, wc0_, wi_;
 
     // mysterious parameters
     float beta_  = 2.0f;
     float alpha_ = 1e-3;
     float k_     = 0;
 
-    float lambda_ = alpha_ * alpha_ * state_dim;
+    float lambda_ = alpha_ * alpha_ * (state_dim + k_);
+  };
+
+  template <typename DataType_, int dim_>
+  struct ManifoldBase {
+    static constexpr int dim = dim_;
+    using DataType           = DataType_;
+    using TangentType        = TangentType_<dim>;
   };
 
 } // namespace test_imu
